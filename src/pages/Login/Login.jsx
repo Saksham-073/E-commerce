ea6d2ef3503b658a2eb21445a9aee29291588c92
Saksham jamwal <130532990+Saksham-073@users.noreply.github.com>
2025-04-12 14:06:29 +0530

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
-import useAuth from '@/hooks/UseAuth';
+import useAuth from '../../hooks/UseAuth';
 import Notification from '../../components/Notification/Notification';
 import './Login.css';
 
