8e4d88ed9fcb051f31762b71962db4b3990e5288
Saksham jamwal <sakshamjamwal723@gmail.com>
2025-04-12 13:56:50 +0530

    added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
-import useAuth from '../../hooks/useAuth';
+import useAuth from '../../hooks/useAuth.jsx';
 import Notification from '../../components/Notification/Notification';
 import './Login.css';
 
